12b3febe3835ef001bb5b0ec3a13404b92f17384
Jameel Jiwani <jameeljiwani@Jameels-MacBook-Air.local>
2021-11-26 02:28:56 -0500

    Unused menu items are removed

--- src/App.js
+++ src/App.js
@@ -76,18 +76,6 @@ function App() {
                 <Menu.Item key="2" icon={<CodeOutlined />}>
                   Queries
                 </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-                  <Menu.Item key="3">Tom</Menu.Item>
-                  <Menu.Item key="4">Bill</Menu.Item>
-                  <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-                  <Menu.Item key="6">Team 1</Menu.Item>
-                  <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined />}>
-                  Files
-                </Menu.Item>
               </Menu>
             </Sider>
             <Layout className="site-layout">
